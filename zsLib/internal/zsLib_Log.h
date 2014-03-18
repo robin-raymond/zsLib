4aa4ea044315b5fe2380a74807ec709ba12258d5
Robin Raymond <robin@hookflash.com>
2014-03-18 17:54:22 -0400

    - Allow selection of subsystem in logging to debugging life easier when dealing with media https://github.com/openpeer/op/issues/78

--- zsLib/internal/zsLib_Log.h
+++ zsLib/internal/zsLib_Log.h
@@ -98,18 +98,31 @@ namespace zsLib
 #define ZS_INTERNAL_FUNCTION_FILE_LINE                                  __FUNCTION__, __FILE__, __LINE__
 
 #define ZS_INTERNAL_GET_LOG_LEVEL()                                     ((ZS_GET_SUBSYSTEM()).getOutputLevel())
+#define ZS_INTERNAL_GET_SUBSYSTEM_LOG_LEVEL(xSubsystem)                 ((xSubsystem).getOutputLevel())
 #define ZS_INTERNAL_IS_LOGGING(xLevel)                                  (((ZS_GET_SUBSYSTEM()).getOutputLevel()) >= ::zsLib::Log::xLevel)
+#define ZS_INTERNAL_IS_SUBSYSTEM_LOGGING(xSubsystem, xLevel)            (((xSubsystem).getOutputLevel()) >= ::zsLib::Log::xLevel)
 
-#define ZS_INTERNAL_LOG_BASIC(xMsg)                                     if (ZS_INTERNAL_IS_LOGGING(Basic))  {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::Basic, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_DETAIL(xMsg)                                    if (ZS_INTERNAL_IS_LOGGING(Detail)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::Detail, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_DEBUG(xMsg)                                     if (ZS_INTERNAL_IS_LOGGING(Debug))  {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::Debug, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_TRACE(xMsg)                                     if (ZS_INTERNAL_IS_LOGGING(Trace))  {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::Trace, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_INSANE(xMsg)                                    if (ZS_INTERNAL_IS_LOGGING(Insane)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::Insane, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_BASIC(xSubsystem, xMsg)               if (ZS_INTERNAL_IS_LOGGING(Basic))  {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::Basic, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_DETAIL(xSubsystem, xMsg)              if (ZS_INTERNAL_IS_LOGGING(Detail)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::Detail, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_DEBUG(xSubsystem, xMsg)               if (ZS_INTERNAL_IS_LOGGING(Debug))  {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::Debug, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_TRACE(xSubsystem, xMsg)               if (ZS_INTERNAL_IS_LOGGING(Trace))  {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::Trace, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_INSANE(xSubsystem, xMsg)              if (ZS_INTERNAL_IS_LOGGING(Insane)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::Insane, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
 
-#define ZS_INTERNAL_LOG(xLevel, xMsg)                                   if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Informational, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_WARNING(xLevel, xMsg)                           if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Warning, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_ERROR(xLevel, xMsg)                             if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Error, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
-#define ZS_INTERNAL_LOG_FATAL(xLevel, xMsg)                             if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log(ZS_GET_SUBSYSTEM(), ::zsLib::Log::Fatal, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_BASIC(xMsg)                                     ZS_INTERNAL_LOG_SUBSYSTEM_BASIC(ZS_GET_SUBSYSTEM(), xMsg)
+#define ZS_INTERNAL_LOG_DETAIL(xMsg)                                    ZS_INTERNAL_LOG_SUBSYSTEM_DETAIL(ZS_GET_SUBSYSTEM(), xMsg)
+#define ZS_INTERNAL_LOG_DEBUG(xMsg)                                     ZS_INTERNAL_LOG_SUBSYSTEM_DEBUG(ZS_GET_SUBSYSTEM(), xMsg)
+#define ZS_INTERNAL_LOG_TRACE(xMsg)                                     ZS_INTERNAL_LOG_SUBSYSTEM_TRACE(ZS_GET_SUBSYSTEM(), xMsg)
+#define ZS_INTERNAL_LOG_INSANE(xMsg)                                    ZS_INTERNAL_LOG_SUBSYSTEM_INSANE(ZS_GET_SUBSYSTEM(), xMsg)
+
+#define ZS_INTERNAL_LOG_SUBSYSTEM(xSubsystem, xLevel, xMsg)             if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Informational, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_WARNING(xSubsystem, xLevel, xMsg)     if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Warning, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_ERROR(xSubsystem, xLevel, xMsg)       if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Error, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+#define ZS_INTERNAL_LOG_SUBSYSTEM_FATAL(xSubsystem, xLevel, xMsg)       if (ZS_INTERNAL_IS_LOGGING(xLevel)) {::zsLib::Log::log((xSubsystem), ::zsLib::Log::Fatal, ::zsLib::Log::xLevel, ::zsLib::Log::Params(xMsg), ZS_INTERNAL_FUNCTION_FILE_LINE);}
+
+#define ZS_INTERNAL_LOG(xLevel, xMsg)                                   ZS_INTERNAL_LOG_SUBSYSTEM(ZS_GET_SUBSYSTEM(), xLevel, xMsg)
+#define ZS_INTERNAL_LOG_WARNING(xLevel, xMsg)                           ZS_INTERNAL_LOG_SUBSYSTEM_WARNING(ZS_GET_SUBSYSTEM(), xLevel, xMsg)
+#define ZS_INTERNAL_LOG_ERROR(xLevel, xMsg)                             ZS_INTERNAL_LOG_SUBSYSTEM_ERROR(ZS_GET_SUBSYSTEM(), xLevel, xMsg)
+#define ZS_INTERNAL_LOG_FATAL(xLevel, xMsg)                             ZS_INTERNAL_LOG_SUBSYSTEM_FATAL(ZS_GET_SUBSYSTEM(), xLevel, xMsg)
 
 #define ZS_INTERNAL_LOG_IF(xCond, xLevel, xMsg)                         if ((xCond) && (ZS_INTERNAL_IS_LOGGING(xLevel))) {ZS_INTERNAL_LOG(xLevel, xMsg)}
 #define ZS_INTERNAL_LOG_WARNING_IF(xCond, xLevel, xMsg)                 if ((xCond) && (ZS_INTERNAL_IS_LOGGING(xLevel))) {ZS_INTERNAL_LOG_WARNING(xLevel, xMsg)}
