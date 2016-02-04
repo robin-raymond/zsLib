a16e3f8c17466bb0da759460bc85b1bcf8c6960b
Robin Raymond <Robin Raymond>
2016-02-04 14:12:40 -0500

    - fix dispatcher exception temporarily

--- zsLib/cpp/zsLib_MessageQueueThreadUsingCurrentGUIMessageQueueForWinRT.cpp
+++ zsLib/cpp/zsLib_MessageQueueThreadUsingCurrentGUIMessageQueueForWinRT.cpp
@@ -62,10 +62,12 @@ namespace zsLib
     MessageQueueThreadPtr MessageQueueThreadUsingCurrentGUIMessageQueueForWindows::singleton()
     {
       CoreDispatcher ^dispatcher = nullptr;
-      if (nullptr != Windows::ApplicationModel::Core::CoreApplication::MainView) {
-        dispatcher = Windows::ApplicationModel::Core::CoreApplication::MainView->Dispatcher;
-      }
-      if (!dispatcher) {
+#define WARNING_FIX_THIS 1
+#define WARNING_FIX_THIS 2
+      //      if (nullptr != Windows::ApplicationModel::Core::CoreApplication::MainView) {
+//        dispatcher = Windows::ApplicationModel::Core::CoreApplication::MainView->Dispatcher;
+//      }
+      if (nullptr != dispatcher) {
         static SingletonLazySharedPtr<MessageQueueThreadBasic> singleton(MessageQueueThreadBasic::create("zsLib.winrt.backgroundDispatcher"));
         return singleton.singleton();
       }
